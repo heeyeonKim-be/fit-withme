02adecc4277e5fa5550e86784b0db5ba0660800f
heeyeon Kim <hykim01@goability.co.kr>
2024-08-19 17:49:22 +0900

    feature : 이용권 기능 초기 커밋

---
+++ src/main/java/com/example/fitwithme/infrastructure/mapper/SubscriptionMapper.java
@@ -0,0 +1,31 @@
+package com.example.fitwithme.infrastructure.mapper;
+
+import com.example.fitwithme.domain.model.Lesson;
+import com.example.fitwithme.domain.model.Reserve;
+import com.example.fitwithme.presentation.dto.request.LessonRequest;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface SubscriptionMapper {
+
+    List<Lesson> findAllLesson(String selectDate, String day);
+
+    Lesson findLessonById(Long lessonId);
+
+    int countCurrentPersonnel(LessonRequest.detail request);
+
+    Long create(LessonRequest.reserve request);
+
+    @Update("UPDATE RESERVE SET CANCELED=1 WHERE RESERVE_ID=#{reserveId}")
+    int cancel(int reserveId);
+
+    List<Reserve> findAllReserveByUserIdAndDate(LessonRequest.reserveList reserveList);
+
+    Lesson findLessonDetailsByLessonId(Long lessonId);
+
+}
---
+++ src/main/java/com/example/fitwithme/presentation/controller/SubscriptionController.java
@@ -0,0 +1,70 @@
+package com.example.fitwithme.presentation.controller;
+
+import com.example.fitwithme.application.service.SubscriptionService;
+import com.example.fitwithme.common.exception.BadRequestException;
+import com.example.fitwithme.common.exception.ErrorStatus;
+import com.example.fitwithme.domain.model.Lesson;
+import com.example.fitwithme.domain.model.Reserve;
+import com.example.fitwithme.jwt.JwtUtil;
+import com.example.fitwithme.presentation.dto.request.LessonRequest;
+import com.example.fitwithme.presentation.dto.response.LessonResponse;
+import com.example.fitwithme.util.DateUtil;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/subscription")
+public class SubscriptionController {
+
+    private final JwtUtil jwtUtil;
+
+    private final SubscriptionService subscriptionService;
+
+    @GetMapping("/search/{selectDate}")
+    public ResponseEntity<List<Lesson>> findLessons(@PathVariable String selectDate, @RequestHeader("ACCESS_TOKEN") String accessToken) {
+        List<Lesson> lessonList = subscriptionService.findLessons(selectDate);
+        return ResponseEntity.ok(lessonList);
+    }
+
+    @GetMapping("/detail")
+    public ResponseEntity<Lesson> findLessonDetail(@RequestBody LessonRequest.detail request) {
+        Lesson lessonData = subscriptionService.findLessonDetail(request);
+        return ResponseEntity.ok(lessonData);
+    }
+
+    @PostMapping("/reserve")
+    public ResponseEntity<LessonResponse.reserve> reserve(@Valid @RequestBody LessonRequest.reserve request, @RequestHeader("ACCESS_TOKEN") String accessToken){
+        String userId = jwtUtil.getUserIdFromToken(accessToken);
+        request.setUserId(userId);
+
+        LessonResponse.reserve reserve = subscriptionService.reserve(request);
+        return ResponseEntity.ok(reserve);
+    }
+
+    @PutMapping("/cancel/{reserveId}")
+    public ResponseEntity<Integer> cancel(@PathVariable int reserveId){
+        boolean isCancelled = subscriptionService.cancel(reserveId);
+        if (isCancelled) {
+            return ResponseEntity.ok(reserveId);
+        } else {
+            throw new BadRequestException(ErrorStatus.CANCEL_FAIL);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Reserve>> findReserveLessons(@RequestHeader("ACCESS_TOKEN") String accessToken) {
+        String today = DateUtil.getToday();
+        String userId = jwtUtil.getUserIdFromToken(accessToken);
+
+        LessonRequest.reserveList reserveList = new LessonRequest.reserveList(today, userId);
+
+        List<Reserve> lessonList = subscriptionService.findReserveLessons(reserveList);
+        return ResponseEntity.ok(lessonList);
+    }
+
+}
